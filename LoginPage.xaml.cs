78aa586bc03818b6ebc5e396ef361a6552505627
R A <73193647+Mangust02@users.noreply.github.com>
2022-04-27 21:57:09 +0500

    login page fixed

--- LoginPage.xaml.cs
+++ LoginPage.xaml.cs
@@ -23,5 +23,10 @@ namespace AssignmentProject
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
