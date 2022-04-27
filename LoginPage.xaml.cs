46d29ec59d307e0af5ccbaef22c150caf17de5b5
R A <73193647+Mangust02@users.noreply.github.com>
2022-04-27 23:07:27 +0500

    login page finished

--- LoginPage.xaml.cs
+++ LoginPage.xaml.cs
@@ -24,5 +24,25 @@ namespace AssignmentProject
             InitializeComponent();
         }
 
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            String username = usr.Text;
+            String password = pass.Password;
+
+            if(string.IsNullOrEmpty(username))
+            {
+               // MessageBox.Show("Username and Password are required");
+               error1.Visibility = Visibility.Visible;
+                error1.Content = "*Username is required";
+            }else if (string.IsNullOrEmpty(password)){ 
+                error2.Visibility = Visibility.Visible;
+                error2.Content = "*Password is required";
+            }
+            else
+            {
+                error1.Visibility = Visibility.Hidden;
+                error2.Visibility= Visibility.Hidden;
+            }
+        }
     }
 }
